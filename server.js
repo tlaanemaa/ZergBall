a1388d42e4933dda357dcaceb3e4e4ceee173e7c
Taavi <taavi.laanemaa@gmail.com>
2014-12-26 17:33:39 +0200

    First publish

--- server.js
+++ server.js
@@ -1,159 +1,329 @@
-#!/bin/env node
-//  OpenShift sample Node application
+// Global variables for players
+playersList = {}; //List to hold all current players
+scoreBoard = {Reds:0, Blues:0}
+playerStartX = 450
+playerStartY = 282
+sideStartOffsetX = 100
+
+// Global variables for ball
+ballMass = 1
+ballMaxForce = 100
+ballVelocityDecay = 0.99
+ballTouchDistance = 30
+playersList['Ball'] = {UID: 'Ball', side: 'Ball', x: 450, y: 282, xVelocity: 0, yVelocity: 0}
+lastPlayerToKick = {name: '', number: '', side: ''}
+moveBallTimeHolder = ''
+gameOn = true
+
+// Variable to hold match time
+matchTime = {min: 0, sec: 20}
+
+// Basic server setup
 var express = require('express');
-var fs      = require('fs');
+var srv = express()
+var http = require('http').Server(srv);
+var io = require('socket.io')(http);
+
+srv.use(express.static(__dirname));
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
 
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+// Start sending field updates
+sendUpdate()
 
-    //  Scope.
-    var self = this;
+// Start calculating ball movements
+MoveBall()
 
+// Start counting match time clock
+setInterval(incMatchTime, 1000)
+sendMatchTime(false)
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+// Socket stuff for receiving data
+io.on('connection', function(socket){
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
-
-
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
+	// New player registration
+	socket.on('askReg', function(data){
+		var UID = socket.id
+		var counts = teamCounts()
+		var pNum = freePlayerNum()
+		if(counts.reds > counts.blues) side = "B"; else side = "R"
+		if(side == "R") var offst = -sideStartOffsetX; else offst = sideStartOffsetX
+		playersList[UID] = {UID: UID, side: side, x: playerStartX+offst, y: playerStartY, xVelocity: 0, yVelocity: 0, name: data.name, number: pNum}
+		
+		// Tell everyone that someone joined
+		var msg = '>> ' + playersList[UID].name + ' (' + playersList[UID].number + ') has joined'
+		socket.broadcast.emit('chatUpdate', {name: '', msg: msg, sysmes: 1});
+		
+		// Send back registration confirmation
+		socket.emit('getReg', {player: playersList[UID], score: scoreBoard})
+		
+		// Send a welcome message to the player
+		if(side == "B") var msgside = "blue"; else var msgside = "red"
+		msg = 'Welcome to the game ' + playersList[UID].name + '. You are on the ' + msgside + ' team and your player number is ' + pNum + '.'
+		socket.emit('chatUpdate', {name: '', msg: msg, sysmes: 3})
+		
+		// Send current match time to player
+		sendMatchTime(socket);
+	});
+	
+	// Player sent its position
+	socket.on('sendPos', function(data){
+		if(typeof data.UID === 'undefined') return
+		if(typeof playersList[data.UID] === 'undefined') return
+		playersList[data.UID] = data
+	});
+	
+	// Player left. Tell everyone that he left and delete entry from playersList
+	socket.on('disconnect', function(){
+		var elem = playersList[socket.id]
+		if(typeof elem !== 'undefined') {
+			var msg = '>> ' + elem.name + ' (' + elem.number + ') has left'
+			io.sockets.emit('chatUpdate', {name: '', msg: msg, sysmes: 2});
+		}
+		delete playersList[socket.id]
+	});
+	
+	// Player sent a chat message. Bounce it back to everyone
+	socket.on('chatSend', function(data){
+		var elem = playersList[socket.id]
+		var nme = '<Unknown>'
+		if(typeof elem !== 'undefined') nme = elem.name + ' (' + elem.number + ')'
+		io.sockets.emit('chatUpdate', {name: nme, msg: data, sysmes: 0});
+	});
+});
 
 
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
+// Send field update and schedule next send
+function sendUpdate(){
+	io.sockets.emit('fieldUpdate',{data: playersList, lastKick: lastPlayerToKick});
+	setTimeout(sendUpdate, 20);
+}
 
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
+// Send match time updates to the players to make sure they are up to date
+function sendMatchTime(socket){
+	if(!socket){
+		io.sockets.emit('timeUpdate', matchTime);
+	} else {
+		socket.emit('timeUpdate', matchTime);
+	}
+	setTimeout(sendMatchTime, 60000);
+}
 
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
+// Calculate team counts from playerList
+function teamCounts(){
+	var k = Object.keys(playersList)
+	var blus = 0
+	var reds = 0
+	for(var i = 0; i < k.length; i++){
+		if(playersList[k[i]].side == "R") reds +=1
+		if(playersList[k[i]].side == "B") blus +=1
+	}
+	return {reds: reds, blues: blus}
+}
 
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
+// Find lowest free player number
+function freePlayerNum() {
+	var nums = subsetObjToArr(playersList, 'number', 'Ball')
+	var outNum = 0
+	if(nums.length == 0){
+		outNum = 1
+	} else {
+		var maxV = Math.max.apply(null,nums)
+		var minV = Math.min.apply(null,nums)
+		for (var i = minV; i <= maxV; ++i){
+			if(nums.indexOf(i) == -1){
+				outNum = i
+				break;
+			}
+		}
+		if(outNum == 0) outNum = maxV + 1
+	}
+	return outNum
+}
 
+// Function to subset an object
+function subsetObjToArr(obj, key, ignore){
+	var k = Object.keys(obj)
+	var arr = []
+	for(var i = 0; i < k.length; i++){
+		if(k[i] == ignore) continue
+		arr.push(obj[k[i]][key])
+	}
+	return arr
+} 
 
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
+///////////////////////////////////////////////////////
+///////////// Ball Calculations ///////////////////////
 
 
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
+// Function to facilitate ball movement
+function MoveBall() {
+	// Get players' effect on ball (each next player gets less weight to avoid ball getting insane force when many players touch it)
+	var ballElem = playersList['Ball']
+	var k = Object.keys(playersList)
+	var kickFixed = false
+	var kickCount = 1
+	var cumEfct = {x: 0, y: 0}
+	for(var i = 0; i < k.length; i++){
+		if(k[i]=='Ball') continue
+		var efct = calcPlayerEffectOnBall(playersList[k[i]], ballElem)
+		if(efct.x != 0 || efct.y != 0){
+			cumEfct.x += efct.x / kickCount
+			cumEfct.y += efct.y / kickCount
+			kickCount += 1
+			if(!kickFixed){
+				kickFixed = true
+				lastPlayerToKick.name = playersList[k[i]].name
+				lastPlayerToKick.number = playersList[k[i]].number
+				lastPlayerToKick.side = playersList[k[i]].side
+			}
+		}
+	}
+	
+	// Update velocities
+	tBallVelX = getVelocity(ballElem.xVelocity, cumEfct.x, ballMass, ballVelocityDecay)
+	tBallVelY = getVelocity(ballElem.yVelocity, cumEfct.y, ballMass, ballVelocityDecay)
+	
+	// Test if would go over edge, if yes then bounce (Allow goal space)
+	// Check if ball is in goal height (Also count ball's radius of 15 for the horizontal bracket)
+	if(ballElem.y + tBallVelY < 312 && ballElem.y + tBallVelY > 254){
+		// Do nothing as ball is in the gate height and is allows to pass through X borders
+	} else {
+		if(ballElem.x + tBallVelX > 838) tBallVelX = -Math.abs(tBallVelX)
+		if(ballElem.y + tBallVelY > 539) tBallVelY = -Math.abs(tBallVelY)
+		if(ballElem.x + tBallVelX < 61) tBallVelX = Math.abs(tBallVelX)
+		if(ballElem.y + tBallVelY < 26) tBallVelY = Math.abs(tBallVelY)
+	}
+	
+	// Check if ball has stopped outside field bounds
+	if(tBallVelX == 0 && tBallVelY == 0){
+		if(ballElem.y >= 563 || ballElem.y <= 0){
+			ballElem.x = 450
+			ballElem.y = 282
+		}
+	}
+	
+	// Save new position
+	playersList['Ball'].x = ballElem.x + tBallVelX
+	playersList['Ball'].y = ballElem.y + tBallVelY
+	playersList['Ball'].xVelocity = tBallVelX
+	playersList['Ball'].yVelocity = tBallVelY	
+	
+	// Register goal if there was one
+	if(playersList['Ball'].x > 853){
+		// Ball is in Blue's gate
+		scoreBoard.Reds += 1
+		sendGoalInf({side: "R", score: scoreBoard, name: lastPlayerToKick.name, number: lastPlayerToKick.number})
+	} else if(playersList['Ball'].x < 46) {
+		// Ball is in Red's gate
+		scoreBoard.Blues += 1
+		sendGoalInf({side: "B", score: scoreBoard, name: lastPlayerToKick.name, number: lastPlayerToKick.number})
+	} else {
+		// No goal -> reschedule next run
+		if(gameOn) moveBallTimeHolder = setTimeout(MoveBall, 20);
+	}
+}
 
+function sendGoalInf(data){
+	// Move ball to center in 4 seconds and continue tracking ball's position
+	setTimeout(function(){
+		playersList['Ball'].x = 450
+		playersList['Ball'].y = 282
+		playersList['Ball'].xVelocity = 0
+		playersList['Ball'].yVelocity = 0
+		io.sockets.emit('continueGame',{gameOn: true});
+		MoveBall()
+	}, 4000)
+	io.sockets.emit('teamScored',data);
+} 
+
+// Function to calculate any player's current effect on the ball
+function calcPlayerEffectOnBall(playerElem, ballElem){
+	var dist = calcDistance(playerElem.x, ballElem.x, playerElem.y, ballElem.y)
+	if(dist < ballTouchDistance){
+		var bounceForce = (ballTouchDistance-dist) / ballTouchDistance * ballMaxForce
+		var dirX = ballElem.x - playerElem.x
+		var dirY = ballElem.y - playerElem.y
+		if(dirX == 0 && dirY == 0) dirY = -1
+		var tot = Math.abs(dirX)+Math.abs(dirY)
+		dirX = (dirX/tot) * bounceForce
+		dirY = (dirY/tot) * bounceForce
+		return {x: dirX, y: dirY}
+	} else return{x: 0, y: 0}
+}
+
+// Calculates velocity from old velocity
+function getVelocity(curVelocity, Force, Mass, VelocityDecay){
+	var newVel = curVelocity * VelocityDecay
+	var a = Force/Mass
+	newVel = newVel + a
+	if(decimalRound(newVel,2) == 0) newVel = 0
+	return newVel
+}
+
+// Simple function to calculate distances
+function calcDistance(x1, x2, y1, y2){
+	return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2))
+}
+
+// Simple rounding function
+function decimalRound(x, n){
+	return Math.round(x*Math.pow(10,n))/Math.pow(10,n)
+}
+
+// Function to count match time
+function incMatchTime(){
+	if(matchTime.sec == 0){
+		matchTime.sec = 59
+		matchTime.min -= 1
+	} else {
+		matchTime.sec -= 1
+	}
+	
+	if(matchTime.sec <= 0 && matchTime.min <= 0){
+		
+		resetMatch()	
+		
+		matchTime.sec = 0
+		matchTime.min = 90
+	}
+}
+
+// Function to reset match when timer hits 0
+function resetMatch(){
+	// Stop updating ball's location
+	clearTimeout(moveBallTimeHolder)
+	gameOn = false
+	
+	// Tell players that reset happened
+	io.sockets.emit('matchReset', {gameOn: gameOn});
+	
+	// Set ball to center
+	playersList['Ball'].x = 450
+	playersList['Ball'].y = 282
+	playersList['Ball'].xVelocity = 0
+	playersList['Ball'].yVelocity = 0
+	
+	// Switch teams
+	var k = Object.keys(playersList)
+	for(var i = 0; i < k.length; i++){
+		if(playersList[k[i]].side == "R"){
+			playersList[k[i]].side = "B"
+		} else if(playersList[k[i]].side == "B") {
+			playersList[k[i]].side = "R"
+		}
+	}
+	
+	// Tell players that sides were switched
+	io.sockets.emit('sideSwitch', playersList);
+	
+	// Schedule game start to 5 seconds
+	setTimeout(function(){
+		gameOn = true
+		io.sockets.emit('continueGame',{gameOn: gameOn});
+		MoveBall()
+	}, 5000)
+}
---
+++ www/js/client.js
@@ -0,0 +1,409 @@
+
+var socket = io();
+
+$( document ).ready(function() {
+	
+	// Set canvas objects
+    var canvas = document.getElementById("mainCanvas")
+    ctx = canvas.getContext("2d");
+	
+	// Movement settings
+	startX = 500
+	startY = 334
+	Force = 10
+	Mass = 10
+	PlayerVelocityDecay = 0.9 // this is a multiplier
+	RefreshRate = 20
+	PlayerColor = "rgba(0,0,0,0.9)" // this gets alter overwritten with data from server
+	
+	// Variables for movement code
+	goLeft = 0
+	goUp = 0
+	goRight = 0
+	goDown = 0
+	
+	// Last time object positions were updated
+	lastUpdate = 0
+	
+	// Ball velocity decay for extrapolation
+	ballVelocityDecay = 0.99
+	
+	// Variable to hold element locations
+	MyLoc = {}
+	RemoteList = {}
+	
+	// Variable to hold match time
+	matchTime = {min: 90, sec: 0}
+	
+	// Variable to hold current game state
+	gameOn = false
+	isRegd = false
+	
+	// Variable to hold who kicked the ball last
+	lastPlayerToKick = {prev: {name: '', number: '', side: ''}, cur: {name: '', number: '', side: ''}}
+	
+	// Keypress events
+	$(document).keydown(function(e) {
+		if(isRegd){
+			if(e.keyCode === 13){
+				// Send chat message
+				var msg = $('#msgBox').val()
+				if(msg != ''){
+					socket.emit('chatSend', msg)
+					$('#msgBox').val('')
+					return false
+				}
+			} else if([37, 38, 39, 40].indexOf(e.keyCode) > -1){
+				setDirVars(e.which, Force)
+				e.preventDefault(); // prevent the default action (scroll / move caret)
+			} else {
+				$('#msgBox').focus();
+			}
+		} else {
+			if(e.keyCode === 13) $('#regSubmit').click()
+		}
+		
+	});
+	
+	$(document).keyup(function(e) {
+		if([37, 38, 39, 40].indexOf(e.keyCode) > -1){
+			setDirVars(e.which, 0)
+			e.preventDefault(); // prevent the default action (scroll / move caret)
+		}
+	});
+		
+	// Set up function to handle field update from server
+	socket.on('fieldUpdate', function(data){
+		lastUpdate = new Date();
+		RemoteList = data.data
+		lastPlayerToKick.cur = data.lastKick
+	 });
+		
+	// Set up function to registration response
+	socket.on('getReg', function(data){
+		// Save player data
+		MyLoc = data.player
+		// Write out current score
+		$('div.scoreBoard div#redScore').text(data.score.Reds)
+		$('div.scoreBoard div#blueScore').text(data.score.Blues)
+		
+		// Start game functions
+		gameOn = true
+		isRegd = true
+		UpdateField()
+		ReDraw()
+		updateLastKick()
+		setInterval(incTime, 1000);
+		
+		// Fade the playfield and other stuff in and registration menu out
+		$('div#regBlock').animate({
+			'margin-top': 140,
+			opacity: 0
+		}, 300, function(){
+			$('div.outCont').animate({
+				'margin-top': 0,
+				opacity: 1
+			}, 300);
+			$('div#regCont').animate({
+				opacity: 0
+			}, 300, function() {
+				$('div#regCont').css({'display':'none'});
+			});
+		});
+		
+	 });
+	
+	// Set up function to handle goal messages
+	socket.on('teamScored', function(data){
+		$('div.scoreBoard div#redScore').text(data.score.Reds)
+		$('div.scoreBoard div#blueScore').text(data.score.Blues)
+		if(data.side == "B"){
+			$('div#scoreAlert div#scoresTxt').text(data.name + ' (' + data.number + ') scores a goal for the Blue team!')
+			$('div#scoreAlert div#scoresTxt').removeClass('forRed forBlue').addClass('forBlue')
+		} else {
+			$('div#scoreAlert div#scoresTxt').text(data.name + ' (' + data.number + ') scores a goal for the Red team!')
+			$('div#scoreAlert div#scoresTxt').removeClass('forBlue forRed').addClass('forRed')
+		}
+		
+		$('div.mainCont div#scoreAlert').css({'display':'block'})
+		$('div.mainCont div#scoreAlert').animate({
+			opacity: 1
+		}, 200)
+		gameOn = data.gameOn
+	 });
+	
+	// Set up function to handle game state changes
+	socket.on('continueGame', function(data){
+		gameOn = data.gameOn
+		$('div.mainCont div#scoreAlert').animate({
+			opacity: 0
+		}, 200, function(){
+			$('div.mainCont div#scoreAlert').css({'display':'block'})
+		})
+	})
+	
+	// Set up function to handle losing focus on window
+	$(window).blur(function() {
+		goLeft = 0
+		goUp = 0
+		goRight = 0
+		goDown = 0
+	})
+	
+	// Fade in the registration window
+	$('div#regBlock').animate({
+		'margin-top': 100,
+		opacity: 1
+	}, 500)
+	
+	// Set focus to the name box on registration form
+	$('#playerName').focus()
+	
+	// Set on click event on registration submit button
+	$('#regSubmit').click(function() {
+		// Ask to be registered on server
+		var nme = $(this).parent().find('#playerName').val()
+		nme = nme.substring(0,1).toUpperCase() + nme.substring(1,200)
+		socket.emit('askReg', {name:nme})
+	});
+	
+	// Set up event to handle chat updates from server
+	socket.on('chatUpdate', function(data){
+		var elem = $('#msgLog')
+		if(data.sysmes == 0){
+			elem.append('<div class="msgElem"><span class="msgElemName">' + data.name + ':</span> ' + data.msg + '</div><br>');
+		} else if(data.sysmes == 1) {
+			elem.append('<div class="msgElem"><span class="msgElemJoin">' + data.msg + '</span></div><br>');
+		} else if(data.sysmes == 2) {
+			elem.append('<div class="msgElem"><span class="msgElemLeave">' + data.msg + '</span></div><br>');
+		} else if(data.sysmes == 3) {
+			elem.append('<div class="msgElem"><span class="msgElemPersn">' + data.msg + '</span></div><br>');
+		}
+		elem.scrollTop(elem[0].scrollHeight)
+	 });
+	 
+	 // Set up event to handle match time updates from server
+	 socket.on('timeUpdate', function(data){
+		matchTime = data
+		incTime()
+	 });
+	
+	// Set up function to handle match resets (timer ran to 0)
+	socket.on('matchReset', function(data){
+		gameOn = data.gameOn
+		console.log("Match reset!!");
+	 });
+	
+	// Set up function to switch player sides
+	socket.on('sideSwitch', function(data){
+		var me = data[MyLoc.UID]
+		if(typeof me !== 'undefined') MyLoc.side = me.side
+	 });
+	
+});
+
+// Master function that calls relevant functions to update field
+function UpdateField(){
+	
+	// Move my character and emit my new location to server
+	MoveObject(MyLoc, 'Me')
+	socket.emit('sendPos', MyLoc);
+	
+	// If last update from other players was ages ago, extrapolate their position. Only allow predicting ball location if game is on.
+	if(new Date() - lastUpdate > RefreshRate){
+		var keysR = Object.keys(RemoteList)
+		var myky = MyLoc.UID
+		for(i=0; i < keysR.length; i++){
+			if(keysR[i]==myky) continue
+			if(keysR[i]=='Ball' && !gameOn) continue
+			MoveObject(RemoteList[keysR[i]], keysR[i]);
+		}
+	}
+		
+	// Schedule rerun of this function
+	setTimeout(UpdateField, RefreshRate);
+}
+
+// Defines keypress effects on player movement
+function setDirVars(key, frc){
+	switch(key){
+		case 37:
+		goLeft=frc;
+		break;
+		
+		case 38:
+		goUp=frc;
+		break;
+		
+		case 39:
+		goRight=frc;
+		break;
+		
+		case 40:
+		goDown=frc;
+		break;
+	}
+}
+
+// Calculates velocity from old velocity
+function getVelocity(curVelocity, Force, Mass, VelocityDecay){
+	var newVel = curVelocity * VelocityDecay
+	var a = Force/Mass
+	newVel = newVel + a
+	if(decimalRound(newVel,2) == 0) newVel = 0
+	return newVel
+}
+
+// Simple rounding function
+function decimalRound(x, n){
+	return Math.round(x*Math.pow(10,n))/Math.pow(10,n)
+}
+
+// Calculates movements for all objects
+function MoveObject(data, id){
+	// Calculate force for this player or other players
+	if(id=='Me'){
+		var xForce = goRight-goLeft
+		var yForce = goDown-goUp
+		var curVelociDecay = PlayerVelocityDecay
+	} else {
+		var xForce = 0
+		var yForce = 0
+		if(id=='Ball') {
+			var curVelociDecay = ballVelocityDecay
+		} else {
+			var curVelociDecay = 1
+		}
+	}
+
+	// Get current position
+	var x = data.x
+	var y = data.y
+	
+	
+	// Update velocities
+	var VelX = getVelocity(data.xVelocity, xForce, Mass, curVelociDecay)
+	var VelY = getVelocity(data.yVelocity, yForce, Mass, curVelociDecay)
+	
+	// Test if would go over edge, if yes then bounce
+	if(x + VelX > 885) VelX = -Math.abs(VelX)
+	if(y + VelY > 548) VelY = -Math.abs(VelY)
+	if(x + VelX < 15) VelX = Math.abs(VelX)
+	if(y + VelY < 15) VelY = Math.abs(VelY)
+	
+	// Calculate new position
+	x = x + VelX
+	y = y + VelY
+	
+	// Save new position
+	if(id=="Me"){
+		MyLoc.x = x
+		MyLoc.y = y
+		MyLoc.xVelocity = VelX
+		MyLoc.yVelocity = VelY
+	} else {
+		RemoteList[id].x = x
+		RemoteList[id].y = y
+		RemoteList[id].xVelocity = VelX
+		RemoteList[id].yVelocity = VelY
+	}
+}
+
+// Redraws the field
+function ReDraw() {
+	var keysR = Object.keys(RemoteList)
+	ctx.clearRect(0,0,1000,688)
+
+	// Draw other players
+	var myky = MyLoc.UID
+	for(i=0; i < keysR.length; i++){
+		if(keysR[i]==myky) continue
+		if(keysR[i]=='Ball') continue
+		
+		var elem = RemoteList[keysR[i]]
+		ctx.beginPath()
+		ctx.arc(elem.x, elem.y, 15, 0, Math.PI*2)
+		ctx.fillStyle = getColor(elem.side)
+		ctx.fill()
+		drawNumber(elem.x, elem.y, elem.number)
+	}
+	
+	// Draw ball
+	var elem = RemoteList['Ball']
+	if(typeof elem !== 'undefined'){
+		var img = new Image();
+		img.src = '/www/img/ball.png'
+		ctx.drawImage(img,elem.x-15,elem.y-15);
+		//ctx.beginPath()
+		//ctx.arc(elem.x, elem.y, 15, 0, Math.PI*2)
+		//ctx.fillStyle = getColor(elem.side)
+		//ctx.fill()
+	}
+	
+	// Draw player
+	ctx.beginPath()
+	ctx.arc(MyLoc.x, MyLoc.y, 15, 0, Math.PI*2)
+	ctx.fillStyle = getColor(MyLoc.side)
+	ctx.fill()
+	drawNumber(MyLoc.x, MyLoc.y, MyLoc.number)
+	
+	// Schedule next run of this function
+	setTimeout(ReDraw, RefreshRate);
+} 
+
+// Returns color code from R or B team value
+function getColor(side){
+	if(side=="R"){
+		return "rgba(150,0,0,0.9)"
+	} else if(side=="B") {
+		return "rgba(0,0,150,0.9)"
+	} else if(side=="Ball") {
+		return "rgba(250,250,250,1)"
+	}
+}
+
+// Function to keep track who kicked the ball last
+function updateLastKick(){
+	if(lastPlayerToKick.cur.number != lastPlayerToKick.prev.number){
+		if(lastPlayerToKick.cur.side == "R"){
+			$('#lastKickBox').removeClass('lastKickRed lastKickBlue').addClass('lastKickRed')
+		} else {
+			$('#lastKickBox').removeClass('lastKickRed lastKickBlue').addClass('lastKickBlue')
+		}
+		$('#lastKickBox').text('Last kick: ' + lastPlayerToKick.cur.name + ' (' + lastPlayerToKick.cur.number + ')')
+		lastPlayerToKick.prev = lastPlayerToKick.cur
+	}
+	setTimeout(updateLastKick, 100)
+}
+
+// Function to draw numbers on player elements
+function drawNumber(x, y, num){
+		ctx.font = "14px Changa One";
+		ctx.textAlign = 'center';
+		ctx.textBaseline = 'middle';
+		ctx.fillStyle = '#FFFFFF';
+		ctx.fillText(num, x, y);
+}
+
+// Function to increment matchTime
+function incTime(){
+	if(matchTime.sec == 0){
+		matchTime.sec = 59
+		matchTime.min -= 1
+	} else {
+		matchTime.sec -= 1
+	}
+	if(matchTime.sec <= 0 && matchTime.min <= 0){
+		matchTime.sec = 0
+		matchTime.min = 90
+	}
+	$('div.scoreBoard div#timeBox').text(leftPad(matchTime.min,2) + ':' + leftPad(matchTime.sec,2))
+}
+
+function leftPad(number, targetLength) {
+    var output = number + '';
+    while (output.length < targetLength) {
+        output = '0' + output;
+    }
+    return output;
+}
